8e62b61df4b5f81a6b15047147f711d45ded1c96
Herbert Holder <herbertholder@Herberts-Air.localdomain>
2022-10-04 09:07:47 -0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
-import anime from 'animejs/lib/anime.es.js';  
 
 
 openModalButtons.forEach(button => {
